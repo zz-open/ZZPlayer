ab30d7537a10cc0551edee958cc1ffe6c353949e
xulei <373045134@qq.com>
2024-09-06 18:15:22 +0800

    脚本自动push

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # web-music-play
 一款沉浸式网页音乐播放器
+
+- [在线示例](music.zdog.top)
--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-
+  markdown: false,
 }, { ignores: ['HeoMusic-2.3/*'] }, { rules: {
   'no-console': 'off',
   'unicorn/consistent-function-scoping': 'off',
